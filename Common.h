4ecd6e76231ffe1cc37e04fcceaff27f39ca2b29
yeren <yerenkl@gmail.com>
2023-05-12 01:04:27 +0300

    Mario can jump

--- Common.h
+++ Common.h
@@ -9,4 +9,8 @@ using namespace sf;
 #define LEFT -1
 #define RIGHT 1
 #define STOP 0
-#define VELOCITY_X 20
+#define UP 2
+#define VELOCITY_Y 10
+#define VELOCITY_X 15
+#define GRAVITY 1
+#define HEIGHT 132
